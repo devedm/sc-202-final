cd9d2f59809e50e34eb54123ff26ca737d3803e7
YeicolCR02 <yeicolsalas2016@gmail.com>
2024-11-16 15:16:41 -0600

    prueba

--- src/projecto_final/Projecto_final.java
+++ src/projecto_final/Projecto_final.java
@@ -11,5 +11,5 @@ public class Projecto_final {
     public static void main(String[] args) {
         InterfazJOption interfaz = new InterfazJOption();
         interfaz.mainMenu();
-    } // Ejemplo
+    } 
 }
